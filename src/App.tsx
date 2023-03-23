621fdbc8d8917f740d5adfcdf3e402435790cc44
mfm <adekschris@gmail.com>
2023-03-23 22:39:24 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ import { Oval } from 'react-loader-spinner';
 //@ts-ignore
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import mapboxgl from '!mapbox-gl'; 
-//@ts-ignore
-// eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+
 //city and coord data
 const data = [
   {
